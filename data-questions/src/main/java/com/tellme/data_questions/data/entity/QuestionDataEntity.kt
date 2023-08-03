25f56680d4f8d0e5908daea6fa49a7900d36764a
Кирилл Иванов <ivanov.ks101@gmail.com>
2023-08-03 12:11:04 +0500

    Implemented mapping from API to DATA, from DATA to DOMAIN

--- data-questions/src/main/java/com/tellme/data_questions/data/entity/QuestionDataEntity.kt
+++ data-questions/src/main/java/com/tellme/data_questions/data/entity/QuestionDataEntity.kt
@@ -1,16 +1,20 @@
 package com.tellme.data_questions.data.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.tellme.data_questions.domain.entity.Question
 import com.tellme.data_questions.network.entity.QuestionApiEntity
 
-//data class QuestionDataEntity(
-//    val objectId: String,
-//    val questionText: String
-//) {
-//    fun toDomain() = Question(objectId = objectId, text = questionText)
-//}
-//
-//fun List<QuestionDataEntity>.toDomain() = this.map { it.toDomain() }
+@Entity(tableName = "questions")
+data class QuestionDataEntity(
+    @PrimaryKey
+    val objectId: String,
+    val questionText: String
+) {
+    fun toDomain() = Question(objectId = objectId, text = questionText)
+}
 
-fun QuestionApiEntity.toDomain() = Question(objectId, questionText)
-fun List<QuestionApiEntity>.toDomain() = this.map { it.toDomain() }
+fun List<QuestionDataEntity>.toDomain() = this.map { it.toDomain() }
+
+fun QuestionApiEntity.toData() = QuestionDataEntity(objectId, questionText)
+fun List<QuestionApiEntity>.toData() = this.map { it.toData() }
