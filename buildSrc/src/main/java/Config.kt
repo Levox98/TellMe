403aee7729da9c2a4816606bbccb1ca1b3176b35
Кирилл Иванов <ivanov.ks101@gmail.com>
2023-09-10 16:58:03 +0500

    Raised target SDK version

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -1,11 +1,11 @@
 object Config {
 
     const val namespace = "com.tellme"
-    const val compileSdkVersion = 33
+    const val compileSdkVersion = 34
 
     const val applicationId = "com.tellme"
     const val minSdkVersion = 26
-    const val targetSdkVersion = 33
+    const val targetSdkVersion = 34
     const val versionCode = 1
     const val versionName = "1.0"
 
