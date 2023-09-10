65207a398de15b61d4c8d99517ab12b65a48a78d
Кирилл Иванов <ivanov.ks101@gmail.com>
2023-09-10 17:41:12 +0500

    Added edge-to-edge compatibility

--- app/src/main/java/com/tellme/MainActivity.kt
+++ app/src/main/java/com/tellme/MainActivity.kt
@@ -3,15 +3,23 @@ package com.tellme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.tellme.core_ui.theme.AppTheme
+import com.tellme.core_ui.theme.transparentSystemBarStyle
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        enableEdgeToEdge(
+            statusBarStyle = transparentSystemBarStyle,
+            navigationBarStyle = transparentSystemBarStyle
+        )
+
         super.onCreate(savedInstanceState)
         setContent {
             AppScreen()
--- feature-questions/src/main/java/com/tellme/feature_questions/screen/QuestionsScreen.kt
+++ feature-questions/src/main/java/com/tellme/feature_questions/screen/QuestionsScreen.kt
@@ -2,19 +2,26 @@ package com.tellme.feature_questions.screen
 
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.tellme.core_ui.components.AppMainHeader
+import com.tellme.core_ui.components.AppSegmentHeader
 import com.tellme.core_ui.theme.AppTheme
 import com.tellme.feature_questions.model.QuestionsScreenAction
 import com.tellme.feature_questions.model.QuestionsScreenEvent
@@ -44,10 +51,6 @@ fun QuestionsScreen(
         }
     }
 
-    if (viewState.value.isLoading) {
-        CircularProgressIndicator(color = AppTheme.colors.primary)
-    }
-
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -57,23 +60,65 @@ fun QuestionsScreen(
         ) {
             item {
                 AppMainHeader(
-                    modifier = Modifier.padding(horizontal = 16.dp),
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .padding(top = 20.dp)
+                        .statusBarsPadding(),
                     mainText = "Hello",
                     secondaryText = "Catherine!"
                 )
             }
 
+            item { Spacer(modifier = Modifier.requiredHeight(30.dp)) }
+
             item {
-                DailyQuestionsPager(
-                    questions = questions,
-                    onQuestionClick = remember {
-                        { question ->
-                            vm.obtainEvent(
-                                QuestionsScreenEvent.GoToAnswerQuestionEvent(questionId = question.objectId)
-                            )
-                        }
-                    }
+                AppSegmentHeader(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp),
+                    headerText = "TELL ME..."
                 )
+            }
+
+            item { Spacer(modifier = Modifier.requiredHeight(20.dp)) }
+
+            item {
+
+                if (viewState.value.isLoading) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        CircularProgressIndicator(color = AppTheme.colors.primary)
+                    }
+                } else {
+                    DailyQuestionsPager(
+                        questions = questions,
+                        onQuestionClick = remember {
+                            { question ->
+                                vm.obtainEvent(
+                                    QuestionsScreenEvent.GoToAnswerQuestionEvent(questionId = question.objectId)
+                                )
+                            }
+                        }
+                    )
+                }
+            }
+
+            item { Spacer(modifier = Modifier.requiredHeight(42.dp)) }
+
+            item {
+                AppSegmentHeader(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp),
+                    headerText = "MOOD"
+                )
+            }
+
+            item { Spacer(modifier = Modifier.requiredHeight(20.dp)) }
+
+            item {
+                //TODO mood graph
             }
         }
     }
