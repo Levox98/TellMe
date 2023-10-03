1d7c8ee32f0166f8eaf0e28f859812078bffbbe9
Кирилл Иванов <ivanov.ks101@gmail.com>
2023-10-03 11:14:57 +0500

    Added module for "main" screen

---
+++ feature-main/src/main/java/com/tellme/feature_main/MainNavigation.kt
@@ -0,0 +1,10 @@
+package com.tellme.feature_main
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+
+fun NavGraphBuilder.addMainScreen(
+    navController: NavHostController
+) {
+    //TODO: main screen navigation
+}
