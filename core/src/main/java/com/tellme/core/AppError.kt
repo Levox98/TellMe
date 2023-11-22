175d226b668e03cf57f5259f8ce0b74714762f79
Кирилл Иванов <ivanov.ks101@gmail.com>
2023-11-22 10:38:41 +0500

    Added more logging to navigation manager

--- core/src/main/java/com/tellme/core/AppError.kt
+++ core/src/main/java/com/tellme/core/AppError.kt
@@ -20,13 +20,20 @@ sealed class AppError(val code: Int, val message: String = "") {
         const val UNKNOWN_ERROR_LOG_TAG = "UNKNOWN_ERROR"
         const val NAV_ERROR_LOG_TAG = "NAV_ERROR"
 
+        /**
+         * Method used for logging and returning errors specific to the app logic
+         * @param code error code
+         * @param message error message, don't forget to describe the error here
+         * @return specific AppError sub-type that has info on the error that occurred
+         * @see AppError
+         */
         fun error(code: Int, message: String = "Empty message"): AppError {
             val data = when (code) {
                 AppErrorData.Navigation.code -> AppErrorData.Navigation
                 else -> AppErrorData.Unknown
             }
 
-            val concatMessage = data.message + message
+            val concatMessage = data.prefix + message
 
             log(data.tag, concatMessage)
 
@@ -42,7 +49,13 @@ sealed class AppError(val code: Int, val message: String = "") {
     }
 }
 
-enum class AppErrorData(val code: Int, val message: String, val tag: String) {
-    Unknown(code = -1, message = "Unknown error: ", tag = AppError.UNKNOWN_ERROR_LOG_TAG),
-    Navigation(code = 1, message = "Navigation error: ", tag = AppError.NAV_ERROR_LOG_TAG)
+/**
+ * Enum class that holds basic data about errors
+ * @param code error code
+ * @param prefix prefix for error message, for clearer indication of the error type
+ * @param tag logging tag
+ */
+enum class AppErrorData(val code: Int, val prefix: String, val tag: String) {
+    Unknown(code = -1, prefix = "Unknown error: ", tag = AppError.UNKNOWN_ERROR_LOG_TAG),
+    Navigation(code = 1, prefix = "Navigation error: ", tag = AppError.NAV_ERROR_LOG_TAG)
 }
